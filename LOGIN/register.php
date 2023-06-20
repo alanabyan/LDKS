0a94681f847898c1d975dc518af61c42f9bb39d8
alan <alanabyan4@gmail.com>
2023-06-19 22:11:24 -1200

    Project LDKS

---
+++ LOGIN/register.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../images/logo-removebg-preview.png" type="image/x-icon">
+    <title>BukaPasar</title>
+    <link rel="stylesheet" href="/LOGIN/login.css">
+</head>
+<body>
+
+    <header>
+        <span><img src="../images/logo-removebg-preview.png" alt=""></span>
+        <span class="judul">BukaPasar</span>
+        <div class="bantuan">
+            Butuh Bantuan?
+        </div>
+    </header>
+
+    <section class="login">
+        <img src="../images/bg.jpg" alt="">
+        <div class="container">
+            <form action="" method="post" class="login-email">
+                <p style="font-size: 2rem;font-weight:850;">Register</p>
+                <div class="input">
+                    <input type="text" placeholder="Username" name="username">
+                </div>
+                <div class="input">
+                    <input type="text" placeholder="Email" name="email">
+                </div>
+                <div class="input">
+                    <input type="text" placeholder="Password" name="password">
+                </div>
+                <div class="input">
+                    <input type="text" placeholder="Password" name="password">
+                </div>
+                <div class="input">
+                    <input type="text" placeholder="Confirm Password" name="cppassword">
+                </div>
+                <div class="input">
+                    <button name="submit" class="btn">Register</button>
+                </div>
+                <p class="login-regitser-text">Have An Account?</p>
+                <a href="index.php">Log In</a>
+            </form>
+        </div>
+    </section>
+    
+</body>
+</html>
